6a24d911e1a6f2278d13266b3a50b00f42866146
Miguel Ferreira <mferreira@shubergphilis.com>
2014-03-05 10:07:21 +0100

    Initialize project

---
+++ src/test/java/com/schubergphilis/cloudstack/ApplicationRunnerTest.java
@@ -0,0 +1,11 @@
+package com.schubergphilis.cloudstack;
+
+import org.junit.Test;
+
+public class ApplicationRunnerTest {
+
+    @Test
+    public void testMain() throws Exception {
+        ApplicationRunner.main(new String[] {});
+    }
+}
